51ac4dab13897536a4bcaf101bfe613e0eb509ea
asserebryanskiy <asserebryanskiy@gmail.com>
2017-09-26 18:30:39 +0300

    added icon

--- src/main/java/badgegenerator/Main.java
+++ src/main/java/badgegenerator/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/FileLoader.fxml"));
         primaryStage.setTitle("Генератор бейджей");
+        primaryStage.getIcons().add(new Image(getClass()
+                .getResourceAsStream("/images/BG_icon.png")));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
