ae130b7fe100a2ce4a0c00ca49c79d6247fa2214
asserebryanskiy <asserebryanskiy@gmail.com>
2018-01-09 22:13:40 +0300

    added try-with-resources block, added logger and alerts if resource load failed

--- src/main/java/badgegenerator/Main.java
+++ src/main/java/badgegenerator/Main.java
@@ -1,36 +1,58 @@
 package badgegenerator;
 
 import badgegenerator.appfilesmanager.HelpMessages;
+import badgegenerator.appfilesmanager.LoggerManager;
 import com.sun.javafx.PlatformUtil;
 import com.sun.javafx.tk.Toolkit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main extends Application {
+    private static Logger logger = Logger.getLogger(Main.class.getSimpleName());
     private static String appFilesDirPath;
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) {
         // load fonts
-        InputStream boldStream = getClass().getResourceAsStream("/fonts/CIRCE-BOLD.OTF");
-        Font.loadFont(boldStream, 13);
-        boldStream.close();
-        InputStream lightStream = getClass().getResourceAsStream("/fonts/CRC35.OTF");
-        Font.loadFont(lightStream, 13);
-        lightStream.close();
+        String bold  = "/fonts/CIRCE-BOLD.OTF";
+        String light = "/fonts/CRC35.OTF";
+        try {
+            loadFont(bold);
+            loadFont(light);
+        } catch (IOException e) {
+            e.printStackTrace();
+            LoggerManager.initializeLogger(logger);
+            logger.log(Level.SEVERE, "Error loading fonts", e);
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось загрузить шрифты Circe." +
+                    "\nИспользуется стандартный системный шрифт.");
+            alert.show();
+        }
 
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FileLoader.fxml"));
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/fxml/FileLoader.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            LoggerManager.initializeLogger(logger);
+            logger.log(Level.SEVERE, "Error loading FileLoader.fxml", e);
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Не удалось запустить программу.");
+            alert.show();
+        }
+        assert root != null;
         primaryStage.setTitle("Генератор бейджей");
         primaryStage.getIcons().add(new Image(getClass()
                 .getResourceAsStream("/images/BG_icon.png")));
@@ -40,6 +62,12 @@ public class Main extends Application {
         HelpMessages.load();
         primaryStage.setOnCloseRequest(event ->
                 Arrays.stream(Logger.getGlobal().getHandlers()).forEach(Handler::close));
+    }
+
+    private void loadFont(String fontName) throws IOException {
+        try (InputStream stream = getClass().getResourceAsStream(fontName)) {
+            Font.loadFont(stream, 13);
+        }
     }
 
     public static double computeStringWidth(String str, Font font) {
